b8918967775ee03459ca089bb36880402394200e
bupt_cabbage~ <33551862+xiaominghd@users.noreply.github.com>
2022-08-25 23:05:19 +0800

    8.25

--- app.py
+++ app.py
@@ -36,13 +36,21 @@ def get():
 @app.route('/travalEdge',methods=['GET','POST'])
 def getdata():
 
+    startmonth=request.args.get('startmonth')
+    endmonth=request.args.get('endmonth')
     type=request.args.get('type')
+    companion=request.args.get('companion')
 
-    datadict = {'美食': 'ACT-Eat', '娱乐': 'ACT-Entertain', '购物': 'ACT-Buy', '旅游': 'ACT-Visit', '住宿': 'ACT-live',
+    typedict = {'美食': 'ACT-Eat', '娱乐': 'ACT-Entertain', '购物': 'ACT-Buy', '旅游': 'ACT-Visit', '住宿': 'ACT-live',
                 '拍照': 'ACT-Shoot'}
-    travel = datadict[type]
-    cypher = "match (n:`" + str(
-        travel) + "`)<-[r1]-(n1:Travel)-[r2:hasEvent]->(n3)-[r3:nextEvent]->(n4) return n1, n3,n4 limit 40"
+    companiondict={'夫妻游':'夫妻','情侣游':'情侣','个人游':'一个人','亲子游':'亲子','朋友游':'和朋友'}
+
+    if(len(type)!=0 and len(companion)!=0):
+        travel_event = typedict[type]
+        travel_companion = companiondict[companion]
+        cypher = "match (n:`" + str(
+            travel_event) + "`)<-[r1]-(n1:Travel{companion:'"+str(travel_companion)+"'})-[r2:hasEvent]->(n3)-[r3:nextEvent]->(n4) return n1, n3,n4 limit 80"
+
     trvelkg = traveledge(cypher)
     nodelist, relationlist = trvelkg.getData()
     node2index = trvelkg.node2index(nodelist)
@@ -54,8 +62,10 @@ def getdata():
         relation.append({'source': node2index[re[0]['name']], 'target': node2index[re[1]['name']], 'name': 'hasEvent'})
         relation.append({'source': node2index[re[0]['name']], 'target': node2index[re[2]['name']], 'name': 'hasEvent'})
         relation.append({'source': node2index[re[1]['name']], 'target': node2index[re[2]['name']], 'name': 'nextEvent'})
+
     storage.update({'node':nodelist})
     storage.update({'relation':relation})
+
     return jsonify({'node':nodelist,'relation':relation})
 
 if __name__ == '__main__':
